16e4e4dd2e7187706ef8f256e745184e50df5e2a
nameispass <94744718+nameispass@users.noreply.github.com>
2024-12-25 22:00:57 +0700

    Update dtb_final.sql

--- Database/dtb_final.sql
+++ Database/dtb_final.sql
@@ -18,9 +18,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `cuaho`
---
 
+--
+CREATE DATABASE IF NOT EXISTS `database` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `database`;
 -- --------------------------------------------------------
 
 --
